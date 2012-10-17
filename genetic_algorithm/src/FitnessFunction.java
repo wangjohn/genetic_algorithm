d0d7f65ce400398108db1ae74f2cd61edd02e628
John Wang <wangjohn@mit.edu>
2012-10-17 11:34:37 -0400

    Started the genetic algorithm code.

---
+++ genetic_algorithm/src/FitnessFunction.java
@@ -0,0 +1,14 @@
+/* interface for making a fitness function */
+public interface FitnessFunction {
+    
+    /**
+     * @param s: specimen to use the fitness function on
+     * 
+     * @requires s != null
+     * @return FitnessScore object which provides a score to evaluate upon
+     * 
+     */
+    public FitnessScore getScore(Specimen s);
+    
+    public 
+}
